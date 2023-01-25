f3599d340960ed4b92e9a01204ab483f106f8685
swaranjali-sr <swaranjali.ingale@gmail.com>
2023-01-25 14:47:22 +0100

    final commit

--- solutions/drf/restaurants/urls.py
+++ solutions/drf/restaurants/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from restaurants.views import RestaurantsList, RestaurantsDetail
+from restaurants.views import RestaurantsList, RestaurantsDetail, RestaurantsListMixins, RestaurantsDetailsMixins
 
 urlpatterns = [
     path('list/', RestaurantsList.as_view()),
     path('detail/<int:pk>', RestaurantsDetail.as_view()),
+    path('list-mixins/', RestaurantsListMixins.as_view()),
+    path('detail-mixins/<int:pk>', RestaurantsDetailsMixins.as_view())
 ]
--- solutions/drf/restaurants/views.py
+++ solutions/drf/restaurants/views.py
@@ -5,6 +5,7 @@ from restaurants.serializer import RestaurantsSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
+from rest_framework import generics, mixins
 
 
 class RestaurantsList(APIView):
@@ -45,3 +46,27 @@ class RestaurantsDetail(APIView):
         restaurant = self.get_restaurant(pk)
         restaurant.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class RestaurantsListMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Restaurants.objects.all()
+    serializer_class = RestaurantsSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+
+class RestaurantsDetailsMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Restaurants.objects.all()
+    serializer_class = RestaurantsSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
